4c1a0bd9169dfb70e152bc43680cbc1e90a7aa76
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2026-01-07 17:00:54 +0545

    backend + frontend

---
+++ chatbot_backend.py
@@ -0,0 +1,37 @@
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+from dotenv import load_dotenv
+import os
+from langchain_groq import ChatGroq
+from langchain_core.messages import HumanMessage, BaseMessage
+from langchain_core.prompts import PromptTemplate
+import operator
+from langgraph.graph.message import add_messages
+from typing import TypedDict, Literal, Annotated
+
+load_dotenv()
+# llm for cht model
+llm = ChatGroq(
+    model=os.getenv("model"),
+    api_key=os.getenv("api_key")
+)
+
+class MessageState(TypedDict):
+    messages : Annotated[list[str], add_messages]
+
+def chatModel(state : MessageState):
+    messages = state['messages']
+    response = llm.invoke(messages)
+    return {'messages' : [response]}
+
+checkpointer = MemorySaver()
+graph = StateGraph(MessageState)
+graph.add_node("chatModel", chatModel)
+graph.add_edge(START, "chatModel")
+graph.add_edge("chatModel", END)
+
+ChatModel = graph.compile(checkpointer=checkpointer)
+
+config = {'configurable' : {'thread_id' : 1}}
+# response = ChatModel.invoke({"messages" : [HumanMessage(content="What is my name")]}, config=config)
+# print(response['messages'][-1].content)
--- chatbot_frontend.py
+++ chatbot_frontend.py
@@ -1,10 +1,13 @@
 import streamlit as st
+from chatbot_backend import ChatModel
+from langchain_core.messages import HumanMessage
+
+config = {'configurable' : {'thread_id' : 1}}
 
 if "message_history" not in st.session_state:
     st.session_state.message_history = []
 
 for i in st.session_state.message_history:
-    st.header("Streamlit")
     with st.chat_message(i['role']):
         st.text(i['message'])
 
@@ -14,6 +17,7 @@ if user_input:
     with st.chat_message("User"):
         st.text(user_input)
     
-    st.session_state.message_history.append({'role' : "AI", "message" : user_input})
+    response = ChatModel.invoke({"messages" : [HumanMessage(content=user_input)]}, config=config)
+    st.session_state.message_history.append({'role' : "AI", "message" : response['messages'][-1].content})
     with st.chat_message("AI"):
-        st.text(user_input)
+        st.text(response['messages'][-1].content)
